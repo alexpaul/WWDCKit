13b8f0c9a656535c74cacbcbc9e9d87112306a8e
Alex Paul <alexpaul@users.noreply.github.com>
2022-04-05 17:28:49 -0400

    Create WWDCView

--- Package.swift
+++ Package.swift
@@ -2,9 +2,13 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import IOSurface
 
 let package = Package(
     name: "WWDCKit",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
---
+++ Sources/WWDCKit/WWDCView.swift
@@ -0,0 +1,20 @@
+//
+//  WWDCView.swift
+//  
+//
+//  Created by Alex Paul on 4/5/22.
+//
+
+import SwiftUI
+
+struct WWDCView: View {
+    var body: some View {
+        Text("WWDC 2022")
+    }
+}
+
+struct WWDCView_Previews: PreviewProvider {
+    static var previews: some View {
+        WWDCView()
+    }
+}
