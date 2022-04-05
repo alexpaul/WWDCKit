17022f788db0772a7271b8cfbf6b14abad5383d7
Alex Paul <alexpaul@users.noreply.github.com>
2022-04-05 18:42:12 -0400

    Added demo project.

---
+++ WWDCPackageDemo/WWDCPackageDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WWDCPackageDemo
+//
+//  Created by Alex Paul on 4/5/22.
+//
+
+import SwiftUI
+import WWDCKit
+
+struct ContentView: View {
+    var body: some View {
+        WWDCView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WWDCPackageDemo/WWDCPackageDemo/WWDCPackageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCPackageDemoApp.swift
+//  WWDCPackageDemo
+//
+//  Created by Alex Paul on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WWDCPackageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
