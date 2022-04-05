82d8fdac4cb1adf4093d0d3c449d9dbee9cfad53
Alex Paul <alexpaul@users.noreply.github.com>
2022-04-05 18:04:35 -0400

    Added custom modifier for Text view styling

--- Sources/WWDCKit/WWDCView.swift
+++ Sources/WWDCKit/WWDCView.swift
@@ -26,17 +26,13 @@ struct WWDCView: View {
                     .font(.largeTitle)
                     .padding(.bottom, 10)
                 Text("Join developers worldwide from June 6 to 10 for an inspiring week of technology and community. Get a first look at Apple’s latest platforms and technologies in sessions, explore the newest tools and tips, and connect with Apple experts in labs and digital lounges. All online and at no cost.")
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 10)
+                    .textStyle()
                 Text("In addition to the online conference, Apple will host a special day for developers and students at Apple Park on June 6 to watch the keynote and State of the Union videos together, along with the online community. Space is limited and details on how to apply to attend will be provided soon.")
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 10)
+                    .textStyle()
                 Text("Wherever you watch from, get ready for a fantastic WWDC.")
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 10)
+                    .textStyle()
                 Text("Talented students can showcase their creativity for the opportunity to receive an award in the [Swift Student Challenge](https://developer.apple.com/wwdc22/swift-student-challenge/).")
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 10)
+                    .textStyle()
             }.padding()
         }
         .foregroundColor(.white)
@@ -47,5 +43,20 @@ struct WWDCView: View {
 struct WWDCView_Previews: PreviewProvider {
     static var previews: some View {
         WWDCView()
+    }
+}
+
+
+struct TextStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .multilineTextAlignment(.center)
+            .padding(.bottom, 10)
+    }
+}
+
+extension View {
+    func textStyle() -> some View {
+        modifier(TextStyle())
     }
 }
